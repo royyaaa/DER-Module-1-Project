521ba3883bd4c69b11b0897e5598c82f6b836325
royyaaa <n.isfakhuraiya@gmail.com>
2026-04-24 00:28:59 +0700

    linting with black

--- fetch_once.py
+++ fetch_once.py
@@ -1,20 +1,22 @@
 from project_module1.fetch_data import build_dataset
 from project_module1.save_data import save_to_json
 
+
 def main():
     # fetching data
-    keywords = ["how to stop brainrot","digital detox" "reduce screen time","stop scrolling addiction","doom scrolling"]
+    keywords = [
+        "how to stop brainrot",
+        "digital detox" "reduce screen time",
+        "stop scrolling addiction",
+        "doom scrolling",
+    ]
     regions = ["ID", "SG", "MY"]
 
     all_data = []
 
     for kw in keywords:
         for reg in regions:
-            data = build_dataset(
-                query=kw,
-                region_code=reg,
-                max_results=500
-            )
+            data = build_dataset(query=kw, region_code=reg, max_results=500)
             all_data.extend(data)
 
     save_to_json(all_data, "data/raw/brainrot464.json")
